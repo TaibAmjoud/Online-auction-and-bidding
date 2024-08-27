2d280c84f86b900d41df9781bebce76c28042364
Taib <taibamjoud5@gmail.com>
2024-08-28 01:23:08 +0200

    Add onChange and onSubmit functionality to the search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,10 +1,96 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Search() {
+  const navigate = useNavigate();
+  const [sidebardata, setSidebardata] = useState({
+    searchTerm: "",
+    type: "all",
+    sort: "created_at",
+    order: "desc",
+  });
+  const [loading, setLoading] = useState(false);
+  const [listing, setListing] = useState([]);
+
+  console.log(listing);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const typeFromUrl = urlParams.get("type");
+    const sortFromUrl = urlParams.get("sort");
+    const orderFromUrl = urlParams.get("order");
+
+    if (searchTermFromUrl || typeFromUrl || sortFromUrl || orderFromUrl) {
+      setSidebardata({
+        searchTerm: searchTermFromUrl || "",
+        type: typeFromUrl || "all",
+        sort: sortFromUrl || "created_at",
+        order: orderFromUrl || "desc",
+      });
+    }
+
+    const fetchListing = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/listing/get?${searchQuery}`);
+      const data = await res.json();
+      setListing(data);
+      setLoading[false];
+    };
+    fetchListing();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (
+      e.target.id === "all" ||
+      e.target.id === "Art/Antiques" ||
+      e.target.id === "Vehicles" ||
+      e.target.id === "Jewelry/Watches" ||
+      e.target.id === "Technology" ||
+      e.target.id === "Immobilier"
+    ) {
+      setSidebardata({
+        ...sidebardata,
+        type: e.target.id,
+      });
+    }
+    if (e.target.id === "searchTerm") {
+      setSidebardata({
+        ...sidebardata,
+        searchTerm: e.target.value,
+      });
+    }
+
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at";
+
+      const order = e.target.value.split("_")[1] || "desc";
+
+      setSidebardata({
+        ...sidebardata,
+        sort,
+        order,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchTerm", sidebardata.searchTerm);
+    urlParams.set("type", sidebardata.type);
+    urlParams.set("sort", sidebardata.sort);
+    urlParams.set("order", sidebardata.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className="mb-36 flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
-        <form className="flex flex-col gap-8">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-8">
           <div className="flex items-center gap-2">
             <label className="whitespace-nowrap font-semibold">
               {" "}
@@ -15,48 +101,91 @@ export default function Search() {
               id="searchTerm"
               placeholder="Search..."
               className="border rounded-lg p-3 w-full"
+              value={sidebardata.searchTerm}
+              onChange={handleChange}
             />
           </div>
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Type:</label>
             <div>
               <div className="flex gap-2">
-                <input type="checkbox" id="all" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="all"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={sidebardata.type === "all"}
+                />
                 <span>All</span>
               </div>
               <div className="flex gap-2">
-                <input type="checkbox" id="Art/Antiques" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="Art/Antiques"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={sidebardata.type === "Art/Antiques"}
+                />
                 <span>Art/Antiques</span>
               </div>
             </div>
             <div>
               <div className="flex gap-2">
-                <input type="checkbox" id="Vehicles" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="Vehicles"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={sidebardata.type === "Vehicles"}
+                />
                 <span>Vehicles</span>
               </div>
               <div className="flex gap-2">
-                <input type="checkbox" id="Jewelry/Watches" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="Jewelry/Watches"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={sidebardata.type === "Jewelry/Watches"}
+                />
                 <span>Jewelry/Watches</span>
               </div>
             </div>
             <div>
               <div className="flex gap-2">
-                <input type="checkbox" id="Technology" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="Technology"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={sidebardata.type === "Technology"}
+                />
                 <span>Technology</span>
               </div>
               <div className="flex gap-2">
-                <input type="checkbox" id="Immobilier" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="Immobilier"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={sidebardata.type === "Immobilier"}
+                />
                 <span>Immobilier</span>
               </div>
             </div>
           </div>
           <div className="flex items-center gap-2">
             <label className="font-semibold">Sort:</label>
-            <select id="sort_order" className="border rounded-lg p-3">
-              <option>Price high to low</option>
-              <option>Price low to hight</option>
-              <option>Latest</option>
-              <option>Oldest</option>
+            <select
+              onChange={handleChange}
+              defaultValue={"created_at_desc"}
+              id="sort_order"
+              className="border rounded-lg p-3"
+            >
+              <option value="regularPrice_desc">Price high to low</option>
+              <option value="regularPrice_asc">Price low to hight</option>
+              <option value="createdAt_desc">Latest</option>
+              <option value="createdAt_asc">Oldest</option>
             </select>
           </div>
           <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
