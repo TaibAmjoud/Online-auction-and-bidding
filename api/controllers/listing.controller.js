dad7f5b989e9b2b7365939d28df8236798133820
Taib <taibamjoud5@gmail.com>
2024-08-29 01:03:44 +0200

    Create update listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -78,3 +78,22 @@ export const getListings = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
